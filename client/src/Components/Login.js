d27622e5edf77f319973dc558e5693da6367f123
Karanveer-Mul <karanveer7171@gmail.com>
2021-02-24 03:10:40 +0530

    Login and Tab setup

---
+++ client/src/Components/Login.js
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+
+const Login = ({ onIdSubmit }) => {
+  const idRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onIdSubmit(idRef.current.value);
+  };
+
+  const createNewId = () => {
+    onIdSubmit(uuidv4());
+  };
+
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: "100vh" }}
+    >
+      <Form className="w-100" onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Enter Your Id</Form.Label>
+          <Form.Control type="text" ref={idRef} required></Form.Control>
+        </Form.Group>
+        <Button type="submit" className="mr-2">
+          Login
+        </Button>
+        <Button variant="secondary" onClick={() => createNewId()}>
+          Signup
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
